e5dd8350a6ff3e216487d1d263761f2c33a89b6a
Kenneth Reitz <me@kennethreitz.org>
2017-01-25 17:40:31 -0500

    lazy setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,48 @@
 """
-Flask-SQLite3
+Flask-Caster: Cast Types of Flask Query Parameters
+==================================================
+
+This simple Flask extension allows you to cast the type of (and assign defaults to) request query parameters in Flask.
+
+Example Usage
 -------------
 
-This is the description for that library
+::
+
+    from flask import Flask
+    from flask_caster import FlaskCaster
+
+    app = Flask(__name__)
+
+    caster = FlaskCaster(app)
+    caster.ints = ['size']
+    caster.booleans = ['json']
+    caster.always = ['json']
+
+This will do a few things:
+
+- Assure that the ``size`` query parameter is always an integer.
+- Assure that the ``json`` query parameter is always an boolean.
+- Assure that the ``json`` query parameter is always present, even if
+  if it wasn't provided by the end-user.
+
+Assignable properties include ``ints``, ``booleans``, ``always``, and ``always_default``. The ``always_default`` property can be set to any value,
+or to a callable, which will receive one keyword argument: ``arg_name``.
+
+For boolean casting, ``0``, ``false``, ``f``, and ``null`` will
+automatically be converted to ``False``.
+
+Installation
+------------
+
+::
+
+    $ pip install Flask-Caster
+
+✨🍰✨
 """
+import sys
+import os
 from setuptools import setup
 
 if sys.argv[-1] == "publish":
